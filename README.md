20a2818e896a37d031c74040b3b41954a6ed543c
karenL26 <karen.lima@estudiantes.utec.edu.uy>
2022-09-13 01:33:27 +0000

    project completed

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Template for Machine Learning projects
+# K-means Project
+> 4 Geeks Academy - project by @karenL26 :elf:
+
+In this small project you will use the k-means algorithm to segment houses based on their coordinates and median income.
+
+You can find some basic exploratory and the data preparation [here](src/explore_k_m.ipynb).
+
+The pipeline construction is [here](src/app.py).
+
+The dataset was obtained from this [link](https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv).
--- src/app.py
+++ src/app.py
@@ -1,4 +1,31 @@
-from utils import db_connect
-engine = db_connect()
+##################################
+#       K-means Project          #
+##################################
+### Load libraries and modules ###
+# Dataframes and matrices ----------------------------------------------
+import pandas as pd
+import numpy as np
+# Machine learning -----------------------------------------------------
+from sklearn.cluster import KMeans
 
-# your code here
+#########################################
+# Data Preprocessing and Transformation #
+#########################################
+# Loading the dataset
+df_raw = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv')
+# Create a copy of the original dataset
+df = df_raw.copy()
+# New dataframe with only the 'latitude', 'longitude' and 'medincome' column
+df = df.loc[:,['Latitude', 'Longitude', 'MedInc']]
+
+#####################
+# K-means algorithm #
+#####################
+# Instantiate the kmeans algorithm
+kmeans = KMeans(n_clusters=3)
+# Create cluster feature and predict the cluster by fitting the 3 columns you have
+df["Cluster"] = kmeans.fit_predict(df)
+# 'cluster' column to 'category' type
+df["Cluster"] = df["Cluster"].astype("category")
+# Clauster centers
+print("The cluster centers are: ",kmeans.cluster_centers_)
